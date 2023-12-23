ad2c608021fb5c3821a866604b9f8a2013471cd3
megha77777 <141833704+megha77777@users.noreply.github.com>
2023-12-23 15:48:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
   // your code here
+	s= s.trim();
+	if(s.length==0){
+		return "";
+	}
+	let arr_word = s.split(" ");
+	return arr_word[0];
 }
 
 // Do not change the code below
